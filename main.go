4b308ecbaddb45a18cda53d21a395cf664b7142c
PollyBreak <mspolinko@gmail.com>
2024-01-07 22:44:57 +0600

    added delete

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 	"strconv"
@@ -46,33 +45,29 @@ var energeticsList []Energetic
 func main() {
 
 	dsn := "host=localhost user=postgres password=222316pb dbname=energetix port=5432 sslmode=disable TimeZone=Asia/Shanghai"
-
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	db.AutoMigrate(&Energetic{}, &Composition{})
-
 	db.Preload("Composition").Find(&energeticsList)
 
 	// createEnergetic(db, "Monster2", "Orange", "Wow", "Ho", "Hungry", "dfsfsfs", 32, 400)
-
-	for _, energetics := range energeticsList {
-		log.Printf("EnergeticsID: %d, Name: %s, Taste: %s, Description: %s, ManufacturerName: %s, ManufactureCountry: %s, Composition: %+v\n",
-			energetics.EnergeticsID, energetics.Name, energetics.Taste, energetics.Description, energetics.ManufacturerName, energetics.ManufactureCountry, energetics.Composition)
-	}
-
-	var energetic1 Energetic
-	db.Preload("Composition").First(&energetic1, 1)
-	log.Println(energetic1)
+	// for _, energetics := range energeticsList {
+	// 	log.Printf("EnergeticsID: %d, Name: %s, Taste: %s, Description: %s, ManufacturerName: %s, ManufactureCountry: %s, Composition: %+v\n",
+	// 		energetics.EnergeticsID, energetics.Name, energetics.Taste, energetics.Description, energetics.ManufacturerName, energetics.ManufactureCountry, energetics.Composition)
+	// }
+	// var energetic1 Energetic
+	// db.Preload("Composition").First(&energetic1, 1)
+	// log.Println(energetic1)
 
 	router := mux.NewRouter()
 	router.HandleFunc("/energetix", getEnergetics).Methods("GET")
-	// myRouter.HandleFunc("/energetics", postEnergetics).Methods("POST")
+	router.HandleFunc("/energetix", postEnergetic).Methods("POST")
 	router.HandleFunc("/energetix/{id}", getEnergeticsById).Methods("GET")
-	router.HandleFunc("/energetix/{id}", updateEnergeticsById).Methods("POST")
+	router.HandleFunc("/energetix/{id}", updateEnergeticsById).Methods("PUT")
+	router.HandleFunc("/energetix/{id}", deleteEnergeticById).Methods("DELETE")
 
 	headers := handlers.AllowedHeaders([]string{"Content-Type", "Authorization"})
 	origins := handlers.AllowedOrigins([]string{"http://localhost:63342"})
@@ -142,12 +137,72 @@ func updateEnergeticsById(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	targetID, err := strconv.ParseUint(params["id"], 10, 64)
 	if err != nil {
-		http.Error(w, "Invalid ID", http.StatusBadRequest)
+		answer := Message{Status: "400", Message: "Incorrect id"}
+		json.NewEncoder(w).Encode(answer)
 		return
 	}
 	var updatedEnergetic Energetic
 	if err := json.NewDecoder(r.Body).Decode(&updatedEnergetic); err != nil {
-		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		answer := Message{Status: "404", Message: "Invalid JSON message"}
+		json.NewEncoder(w).Encode(answer)
+		return
+	}
+
+	dsn := "host=localhost user=postgres password=222316pb dbname=energetix port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	var existingEnergetic Energetic
+	if err := db.First(&existingEnergetic, targetID).Error; err != nil {
+		answer := Message{Status: "404", Message: "Energy drink with such ID does not exist"}
+		json.NewEncoder(w).Encode(answer)
+		return
+	}
+
+	db.Model(&existingEnergetic).Updates(updatedEnergetic)
+	db.Preload("Composition").Find(&energeticsList)
+	sqlDB, err := db.DB()
+	sqlDB.Close()
+
+	w.WriteHeader(http.StatusOK)
+	answer := Message{Status: "200", Message: "Energy drink was updated"}
+	json.NewEncoder(w).Encode(answer)
+	return
+}
+
+func postEnergetic(w http.ResponseWriter, r *http.Request) {
+	var newEnergetic Energetic
+	if err := json.NewDecoder(r.Body).Decode(&newEnergetic); err != nil {
+		answer := Message{Status: "404", Message: "Invalid JSON message"}
+		json.NewEncoder(w).Encode(answer)
+		return
+	}
+
+	dsn := "host=localhost user=postgres password=222316pb dbname=energetix port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := db.Create(&newEnergetic).Error; err != nil {
+		answer := Message{Status: "404", Message: "Invalid JSON message"}
+		json.NewEncoder(w).Encode(answer)
+		return
+	}
+	db.Preload("Composition").Find(&energeticsList)
+	sqlDB, err := db.DB()
+	sqlDB.Close()
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(newEnergetic)
+}
+
+func deleteEnergeticById(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	targetID, err := strconv.ParseUint(params["id"], 10, 64)
+	if err != nil {
+		answer := Message{Status: "400", Message: "Incorrect id"}
+		json.NewEncoder(w).Encode(answer)
 		return
 	}
 
@@ -157,42 +212,16 @@ func updateEnergeticsById(w http.ResponseWriter, r *http.Request) {
 		log.Fatal(err)
 	}
 
-	// Проверяем, существует ли энергетик с указанным ID
-	var existingEnergetic Energetic
-	if err := db.First(&existingEnergetic, targetID).Error; err != nil {
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
-	}
-
-	// Обновляем данные
-	db.Model(&existingEnergetic).Updates(updatedEnergetic)
-	w.WriteHeader(http.StatusOK)
-
-}
-
-func isEnergeticInList(energeticsList []Energetic, targetID uint) bool {
-	for _, energetics := range energeticsList {
-		if energetics.EnergeticsID == targetID {
-			return true
-		}
-	}
-	return false
-}
-
-func postEnergetic(w http.ResponseWriter, r *http.Request) {
-
-	var message Message
-	err := json.NewDecoder(r.Body).Decode(&message)
-	if err != nil || message.Message == "" {
-		answer := Message{Status: "400", Message: "Invalid JSON message"}
+	if err := db.Delete(&Energetic{}, targetID).Error; err != nil {
+		answer := Message{Status: "404", Message: "Invalid JSON message"}
 		json.NewEncoder(w).Encode(answer)
 		return
 	}
+	db.Preload("Composition").Find(&energeticsList)
+	sqlDB, err := db.DB()
+	sqlDB.Close()
 
-	fmt.Println(message.Message)
-
-	fmt.Printf("Message: %v", message.Message)
-	answer := Message{"Success", "Data successfully received"}
+	answer := Message{Status: "410", Message: "Energy drink was deleted successfully"}
 	json.NewEncoder(w).Encode(answer)
-
+	w.WriteHeader(http.StatusOK)
 }
