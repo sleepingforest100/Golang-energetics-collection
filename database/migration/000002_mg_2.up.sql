db6df182a1530534d9d3eff3e21f70105d0f8ff6
PollyBreak <mspolinko@gmail.com>
2024-01-07 22:46:25 +0600

    added cascade

--- database/migration/000002_mg_2.up.sql
+++ database/migration/000002_mg_2.up.sql
@@ -4,9 +4,9 @@ CREATE TABLE compositions
     energetics_id integer,
     caffeine integer,
     taurine integer,
-	FOREIGN KEY (energetics_id) REFERENCES energetics (energetics_id)
+	FOREIGN KEY (energetics_id) REFERENCES energetics (energetics_id) ON DELETE CASCADE
 );
 
 ALTER TABLE IF EXISTS energetics
     ADD COLUMN composition_id bigint
-	REFERENCES compositions(composition_id);
+	REFERENCES compositions(composition_id) ON DELETE CASCADE;
