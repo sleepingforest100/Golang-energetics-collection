3ee3748ccf221de7ecac0eac327df85e6c09cf9a
PollyBreak <mspolinko@gmail.com>
2024-03-02 17:37:49 +0600

    added reset password

--- models/claims.go
+++ models/claims.go
@@ -3,6 +3,7 @@ package models
 import "github.com/dgrijalva/jwt-go"
 
 type Claims struct {
-	Role string `json:"role"`
+	Role  string `json:"role"`
+	Email string `json:"email"`
 	jwt.StandardClaims
 }
